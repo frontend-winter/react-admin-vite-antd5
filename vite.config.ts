2137b4b5bc3f01ff06fd7a03b405d1d15a6f58c3
winter <123>
2022-12-02 18:00:12 +0800

    feat(add gzip): add gzip
    a
    
    a

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,19 @@ import react from "@vitejs/plugin-react";
 import { fileURLToPath } from "url";
 import { defineConfig } from "vite";
 // https://vitejs.dev/config/
+import vitePluginCompression from "vite-plugin-compression";
 
 export default defineConfig(config => {
   console.log(config, "config");
   return {
-    plugins: [legacy(), react()],
+    plugins: [
+      legacy(),
+      react(),
+      vitePluginCompression({
+        threshold: 1024 * 10, // 对大于 10kb 的文件进行压缩
+        deleteOriginFile: true,
+      }),
+    ],
     resolve: {
       alias: {
         // for TypeScript path alias import like : @/x/y/z
