cebaa93dc6046c2c256dc622f6c8e2a0a8a11d7f
winter <123>
2022-12-05 11:34:35 +0800

    feat: dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,12 @@
-import { Avatar, Statistic } from "antd";
+import { Avatar, Col, Row, Statistic } from "antd";
 import type { FC } from "react";
 
-// import { useSelector } from "react-redux";
-// import { IUserInitialState } from "@/store/reducers/user";
 import { currentTimeRange } from "@/common/utils/common";
 
 import styles from "./style.module.scss";
+import { PageContainer } from "@ant-design/pro-components";
+import { IUserInitialState } from "@/store/reducers/user";
+import { useSelector } from "react-redux";
 
 const PageHeaderContent: FC<{ currentUser: any }> = ({ currentUser }) => {
   return (
@@ -41,55 +42,55 @@ const ExtraContent: FC<Record<string, any>> = () => (
 );
 
 const Workplace: FC = () => {
-  // const { user } = useSelector(state => state) as { user: IUserInitialState };
+  const { user } = useSelector(state => state) as { user: IUserInitialState };
 
-  // const username =
-  //   (user.token as unknown as { username: string })?.username ?? "visitor";
+  const username =
+    (user.token as unknown as { username: string })?.username ?? "visitor";
   return (
-    // <PageContainer
-    //   waterMarkProps={{ content: username }}
-    //   style={{ background: "#fff" }}
-    //   content={
-    //     <div style={{ display: "flex", justifyContent: "space-between" }}>
-    //       <PageHeaderContent
-    //         currentUser={{
-    //           avatar:
-    //             "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
-    //           name: "吴彦祖",
-    //           userid: "00000001",
-    //           email: "antdesign@alipay.com",
-    //           signature: "海纳百川，有容乃大",
-    //           title: "交互专家",
-    //           group: "某某某事业群－某某平台部－某某技术部－UED",
-    //         }}
-    //       />
-    //       <ExtraContent />
-    //     </div>
-    //   }
-    // >
-    //   <Row gutter={24}>
-    //     <Col xl={8} lg={24} md={24} sm={24} xs={24}>
-    //       Hello word
-    //     </Col>
-    //   </Row>
-    // </PageContainer>
-    <div style={{ background: "#fff" }}>
-      <div style={{ display: "flex", justifyContent: "space-between" }}>
-        <PageHeaderContent
-          currentUser={{
-            avatar:
-              "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
-            name: "吴彦祖",
-            userid: "00000001",
-            email: "antdesign@alipay.com",
-            signature: "海纳百川，有容乃大",
-            title: "交互专家",
-            group: "某某某事业群－某某平台部－某某技术部－UED",
-          }}
-        />
-        <ExtraContent />
-      </div>
-    </div>
+    <PageContainer
+      waterMarkProps={{ content: username }}
+      style={{ background: "#fff" }}
+      content={
+        <div style={{ display: "flex", justifyContent: "space-between" }}>
+          <PageHeaderContent
+            currentUser={{
+              avatar:
+                "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+              name: "吴彦祖",
+              userid: "00000001",
+              email: "antdesign@alipay.com",
+              signature: "海纳百川，有容乃大",
+              title: "交互专家",
+              group: "某某某事业群－某某平台部－某某技术部－UED",
+            }}
+          />
+          <ExtraContent />
+        </div>
+      }
+    >
+      <Row gutter={24}>
+        <Col xl={8} lg={24} md={24} sm={24} xs={24}>
+          Hello word
+        </Col>
+      </Row>
+    </PageContainer>
+    // <div style={{ background: "#fff" }}>
+    //   <div style={{ display: "flex", justifyContent: "space-between" }}>
+    //     <PageHeaderContent
+    //       currentUser={{
+    //         avatar:
+    //           "https://gw.alipayobjects.com/zos/rmsportal/BiazfanxmamNRoxxVxka.png",
+    //         name: "吴彦祖",
+    //         userid: "00000001",
+    //         email: "antdesign@alipay.com",
+    //         signature: "海纳百川，有容乃大",
+    //         title: "交互专家",
+    //         group: "某某某事业群－某某平台部－某某技术部－UED",
+    //       }}
+    //     />
+    //     <ExtraContent />
+    //   </div>
+    // </div>
   );
 };
 export default Workplace;
