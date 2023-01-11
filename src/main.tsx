09d8d900d1449415aba984e056cb952f10ead320
winter <1219809916@qq.com>
2023-01-11 15:39:18 +0800

    clear Space

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,7 @@
 import "./main.css";
-
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
 import App from "./app";
 import store from "./store";
 
