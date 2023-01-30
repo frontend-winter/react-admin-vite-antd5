7747d4bfd103abab54940921d10fc399a8f05713
winter <1219809916@qq.com>
2023-01-30 16:46:27 +0800

    fix: Text

--- src/components/Text/index.tsx
+++ src/components/Text/index.tsx
@@ -2,8 +2,8 @@ import { Typography } from "antd";
 import { TextProps } from "antd/es/typography/Text";
 import React, { memo } from "react";
 
-function Text({ children }: TextProps) {
-  return <Typography.Text>{children}</Typography.Text>;
-}
+const Text = ({ children, ...props }: TextProps) => (
+  <Typography.Text {...props}>{children}</Typography.Text>
+);
 
 export default memo(Text);
