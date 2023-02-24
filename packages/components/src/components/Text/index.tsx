c3236085d151a9e64ed5f35d8e0f108a3f0c716d
winter <1219809916@qq.com>
2023-02-24 14:26:29 +0800

    chore: clean

--- packages/components/src/components/Text/index.tsx
+++ packages/components/src/components/Text/index.tsx
@@ -1,6 +1,6 @@
 import { Typography } from "antd";
 import { TextProps } from "antd/es/typography/Text";
-import { memo } from "react";
+import React, { memo } from "react";
 
 const Text = ({ children, ...props }: TextProps) => (
   <Typography.Text {...props}>{children}</Typography.Text>
